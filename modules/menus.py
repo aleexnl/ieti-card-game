65efeb8d0a4ba88437763f699d7e31417fbe775b
alejandro <alex.nieto0027@gmail.com>
2020-02-15 12:43:29 +0100

    -> Añadido menus.py -> Modificadas 5 cartas a myBaraja.xml(ikerfu) -> Tests de cargas de .xml y .dtd en userinfo.py

---
+++ modules/menus.py
@@ -0,0 +1,19 @@
+def inital_menu():
+    print('1. Cargar cartas')
+    print('2. Carga cartas Enemigo')
+
+
+def charged_cards_menu():
+    print('1. Cargar cartas')
+    print('2. Carga cartas Enemigo')
+    print('3. Crear mazo aleatorio')
+    print('4. Crear mazo ofensivo')
+    print('5. Crear mazo defensivo')
+    print('6. Crear mazo equilibrado')
+    print('7. Crear mazo aleatorio Enemigo')
+    print('8. Crear mazo ofensivo Enemigo')
+    print('9. Crear mazo defensivo Enemigo')
+    print('10. Crear mazo equilibrado Enemigo')
+    print('11. Luchar Jugador vs Jugador')
+    print('12. Luchar Jugador vs Bot (arcade)')
+    print('13. Luchar Jugador vs Bot (liga)')
