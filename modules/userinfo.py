0c6a60ca0996973e6fb5efbea01e3074ac0236ed
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-02-17 12:43:14 +0100

    -> Defined function charge_dtd() in userinfo.py -> Defined function charge_user_deck() in userinfo.py

--- modules/userinfo.py
+++ modules/userinfo.py
@@ -1,18 +1,23 @@
 from lxml import etree
 from lxml.etree import DTDParseError
 
-try:
-    dtd = etree.DTD('config/IETI_Card_Game.DTD')
-except DTDParseError:
-    print('ERROR: IETI_Card_Game.DTD no encontrado en el directorio config')
-try:
-    user_deck = etree.parse('decks/myBaraja.xml')
-except OSError:
-    print('ERROR:myBaraja.xml no encontrado en el directorio decks')
+
+def charge_dtd():
+    try:
+        dtd = etree.DTD('config/IETI_Card_Game.DTD')
+    except DTDParseError:
+        print('ERROR: IETI_Card_Game.DTD no encontrado en el directorio config')
+
+
+def charge_user_deck():
+    try:
+        user_deck = etree.parse('decks/myBaraja.xml')
+    except OSError:
+        print('ERROR:myBaraja.xml no encontrado en el directorio decks')
 # try:
 #     enemy_deck = etree.parse('decks/Enemigo.xml')
 # except OSError:
 #     print('ERROR: Enemigo.xml no encontrado en el directorio decks')
 
-if dtd.validate(user_deck) is True:
-    print('validation succesful')
+# if dtd.validate(user_deck) is True:
+#     print('validation succesful')
