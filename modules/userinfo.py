e920de35c161fd771dc19fb85f6fc624952a518a
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-02-17 11:47:41 +0100

    -> Corrected routes to files in userinfo.py -> added imports to main.py

--- modules/userinfo.py
+++ modules/userinfo.py
@@ -2,15 +2,15 @@ from lxml import etree
 from lxml.etree import DTDParseError
 
 try:
-    dtd = etree.DTD('../config/IETI_Card_Game.DTD')
+    dtd = etree.DTD('config/IETI_Card_Game.DTD')
 except DTDParseError:
     print('ERROR: IETI_Card_Game.DTD no encontrado en el directorio config')
 try:
-    user_deck = etree.parse('../decks/myBaraja.xml')
+    user_deck = etree.parse('decks/myBaraja.xml')
 except OSError:
     print('ERROR:myBaraja.xml no encontrado en el directorio decks')
 # try:
-#     enemy_deck = etree.parse('../decks/Enemigo.xml')
+#     enemy_deck = etree.parse('decks/Enemigo.xml')
 # except OSError:
 #     print('ERROR: Enemigo.xml no encontrado en el directorio decks')
 
