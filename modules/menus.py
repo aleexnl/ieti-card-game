a1c7e406e2ccd721033f76a40a5e9309d3ecdc59
sergio-rins <sergiorins@gmail.com>
2020-02-19 09:22:38 +0100

    5 cartas nuevas para la baraja

--- modules/menus.py
+++ modules/menus.py
@@ -42,3 +42,50 @@ def charged_cards_menu():
 #      except UnboundLocalError:
 #          print("ATENCIÓ:XML no carga correctamente")
 #
+
+def menu_carg():
+    print('1. Cargar cartas')
+    print('2. Carga cartas Enemigo')
+
+def menu_mazpro():
+    print('1. Cargar cartas')
+    print('2. Carga cartas Enemigo')
+    print('3. Crear mazo aleatorio')
+    print('4. Crear mazo ofensivo')
+    print('5. Crear mazo defensivo')
+    print('6. Crear mazo equilibrado')
+
+def menu_mazene():
+    print('1. Cargar cartas')
+    print('2. Carga cartas Enemigo')
+    print('7. Crear mazo aleatorio Enemigo')
+    print('8. Crear mazo ofensivo Enemigo')
+    print('9. Crear mazo defensivo Enemigo')
+    print('10. Crear mazo equilibrado Enemigo')
+
+def menu_maztot():
+    print('1. Cargar cartas')
+    print('2. Carga cartas Enemigo')
+    print('3. Crear mazo aleatorio')
+    print('4. Crear mazo ofensivo')
+    print('5. Crear mazo defensivo')
+    print('6. Crear mazo equilibrado')
+    print('7. Crear mazo aleatorio Enemigo')
+    print('8. Crear mazo ofensivo Enemigo')
+    print('9. Crear mazo defensivo Enemigo')
+    print('10. Crear mazo equilibrado Enemigo')
+
+def menu_lucha():
+    print('1. Cargar cartas')
+    print('2. Carga cartas Enemigo')
+    print('3. Crear mazo aleatorio')
+    print('4. Crear mazo ofensivo')
+    print('5. Crear mazo defensivo')
+    print('6. Crear mazo equilibrado')
+    print('7. Crear mazo aleatorio Enemigo')
+    print('8. Crear mazo ofensivo Enemigo')
+    print('9. Crear mazo defensivo Enemigo')
+    print('10. Crear mazo equilibrado Enemigo')
+    print('11. Luchar Jugador vs Jugador')
+    print('12. Luchar Jugador vs Bot (arcade)')
+    print('13. Luchar Jugador vs Bot (liga)')
