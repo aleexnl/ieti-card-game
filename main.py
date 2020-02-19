c3c9d227b11bcfb3da7e69a3c7c85c6adb069dbe
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-02-19 10:30:33 +0100

    -> userinfo.py: Añadidos returns para las funciones y variables globales que te permiten controlar el estado de las cargas de archivos.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # Importamos los archivos menus y userinfo
 from modules import menus, userinfo
 
-menus.inital_menu()
-
+while True:
+    if userinfo.user_deck_charged is False and userinfo.enemy_deck_charged is False:
+        menus.inital_menu()
+    elif userinfo.user_deck_charged is True and userinfo.enemy_deck_charged is False:
+        menus.user_deck_charged()
+    elif userinfo.user_deck_charged is False and userinfo.enemy_deck_charged is True:
+        menus.enemy_deck_charged()
+    elif userinfo.user_deck_charged is True and userinfo.enemy_deck_charged is True:
+        menus.all_decks_charged()
