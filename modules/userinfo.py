80a36f5d92362d4c29968914b60e62e9299b8f60
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-02-19 09:34:05 +0100

    -> userinfo.py: Añadidos returns para las funciones y variables globales que te permiten controlar el estado de las cargas de archivos.

--- modules/userinfo.py
+++ modules/userinfo.py
@@ -1,44 +1,53 @@
 from lxml import etree
 from lxml.etree import DTDParseError
 
+dtd_charged = False
+user_deck_charged = False
+enemy_deck_charged = False
+
 
 def charge_dtd():
+    global dtd_charged
     try:
         dtd = etree.DTD('config/IETI_Card_Game.DTD')
         print('INFO: IETI_Card_Game.DTD cargado correctamente')
         dtd_charged = True
     except DTDParseError:
         print('ERROR: IETI_Card_Game.DTD no encontrado en el directorio config')
-        dtd_charged = False
     if dtd_charged is True:
         return dtd
 
 
 def charge_user_deck(dtd):
+    global user_deck_charged
     try:
         user_deck = etree.parse('decks/myBaraja.xml')
         user_deck_charged = True
+        return user_deck
     except OSError:
         print('ERROR: myBaraja.xml no encontrado en el directorio decks')
-        user_deck_charged = False
     if user_deck_charged is True:
         try:
             if dtd.validate(user_deck) is True:
                 print('INFO: myBaraja.xml validado correctamente')
         except NameError:
             print('ERROR: No ha sido posible validar myBaraja.xml')
+            user_deck_charged = False
 
 
 def charge_enemy_deck(dtd):
+    global enemy_deck_charged
     try:
         enemy_deck = etree.parse('decks/Enemigo.xml')
         enemy_deck_charged = True
+        return enemy_deck
     except OSError:
         print('ERROR: Enemigo.xml no encontrado en el directorio decks')
-        enemy_deck_charged = False
     if enemy_deck_charged is True:
         try:
             if dtd.validate(enemy_deck) is True:
                 print('INFO: Enemigo.xml validado correctamente')
         except NameError:
             print('ERROR: No ha sido posible validar Enemigo.xml')
+            enemy_deck_charged = False
+
