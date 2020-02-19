f6e3e0b3a1a2aa924a407b41dbfc644028b28944
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-02-19 09:02:30 +0100

    -> Carga y validación de dtd añadida en userinfo.py/charge_dtd(), charge_user_deck(), charge_enemy_deck()

--- modules/userinfo.py
+++ modules/userinfo.py
@@ -1,26 +1,42 @@
-# De l'interpret lxml importem etree y despres de lxml.etree importem DTDParseError
 from lxml import etree
 from lxml.etree import DTDParseError
 
-# Esta funcion carga el DTD, si no lo consigue printara un mensaje de error.
+
 def charge_dtd():
     try:
-        dtd = etree.DTD('config/IETI_Card_Game.DTD')
+        dtd = etree.DTD('../config/IETI_Card_Game.DTD')
+        print('INFO: IETI_Card_Game.DTD cargado correctamente')
+        dtd_charged = True
     except DTDParseError:
         print('ERROR: IETI_Card_Game.DTD no encontrado en el directorio config')
+        dtd_charged = False
 
-# Esta funcion carga el XML, si no lo consigue printara un mensaje de error.
-def charge_user_deck():
+
+def charge_user_deck(dtd):
     try:
-        user_deck = etree.parse('decks/myBaraja.xml')
+        user_deck = etree.parse('../decks/myBaraja.xml')
+        user_deck_charged = True
     except OSError:
-        print('ERROR:myBaraja.xml no encontrado en el directorio decks')
-    finally:
-        return user_deck
-# try:
-#     enemy_deck = etree.parse('decks/Enemigo.xml')
-# except OSError:
-#     print('ERROR: Enemigo.xml no encontrado en el directorio decks')
+        print('ERROR: myBaraja.xml no encontrado en el directorio decks')
+        user_deck_charged = False
+    if user_deck_charged is True:
+        try:
+            if dtd.validate(user_deck) is True:
+                print('INFO: myBaraja.xml validado correctamente')
+        except NameError:
+            print('ERROR: No ha sido posible validar myBaraja.xml')
 
-# if dtd.validate(user_deck) is True:
-#     print('validation succesful')
+
+def charge_enemy_deck(dtd):
+    try:
+        enemy_deck = etree.parse('../decks/Enemigo.xml')
+        enemy_deck_charged = True
+    except OSError:
+        print('ERROR: Enemigo.xml no encontrado en el directorio decks')
+        enemy_deck_charged = False
+    if enemy_deck_charged is True:
+        try:
+            if dtd.validate(enemy_deck) is True:
+                print('INFO: Enemigo.xml validado correctamente')
+        except NameError:
+            print('ERROR: No ha sido posible validar Enemigo.xml')
