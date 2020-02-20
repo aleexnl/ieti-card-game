dcd348c14299b2d1015c2abd5d755ded5d8d40e8
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-02-20 08:14:13 +0100

    -> userinfo.py: Solucionado bug de validación de .dtd

--- modules/userinfo.py
+++ modules/userinfo.py
@@ -21,16 +21,18 @@ def charge_user_deck(dtd):
     global user_deck_charged
     try:
         user_deck = etree.parse('decks/myBaraja.xml')
+        user_deck_charged = True
         print('INFO: myBaraja.xml cargado correctamente')
         try:
             if dtd.validate(user_deck) is True:
                 print('INFO: myBaraja.xml validado correctamente')
-                user_deck_charged = True
                 return user_deck
         except NameError:
             print('ERROR: No ha sido posible validar myBaraja.xml')
+            user_deck_charged = False
         except AttributeError:
             print('ERROR: No ha sido posible validar myBaraja.xml')
+            user_deck_charged = False
     except OSError:
         print('ERROR: myBaraja.xml no encontrado en el directorio decks')
 
@@ -39,17 +41,18 @@ def charge_enemy_deck(dtd):
     global enemy_deck_charged
     try:
         enemy_deck = etree.parse('decks/Enemigo.xml')
+        enemy_deck_charged = True
         print('INFO: Enemigo.xml cargado correctamente')
         try:
             if dtd.validate(enemy_deck) is True:
                 print('INFO: Enemigo.xml validado correctamente')
-                enemy_deck_charged = True
                 return enemy_deck
         except NameError:
             print('ERROR: No ha sido posible validar Enemigo.xml')
+            enemy_deck_charged = False
         except AttributeError:
             print('ERROR: No ha sido posible validar Enemigo.xml')
-
+            enemy_deck_charged = False
     except OSError:
         print('ERROR: Enemigo.xml no encontrado en el directorio decks')
 
