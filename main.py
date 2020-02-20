79c0aadf41acbd32bdf78995c69a3ecfac824e72
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-02-20 14:34:36 +0100

    -> PEP-8 Parsed.

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from modules import menus, userinfo
 while True:
     if userinfo.user_deck_charged is False and userinfo.enemy_deck_charged is False:
         menus.inital_menu()
-##Comprobamos que ninguno de los dos mazos este cargado, y printamos por pantalla el primer menu que permite cargar ambos mazos
+    # Comprobamos que ninguno de los dos mazos este cargado, y printamos por pantalla el primer menu que permite cargar
+    # ambos mazos
     elif userinfo.user_deck_charged is True and userinfo.enemy_deck_charged is False:
         menus.user_deck_charged()
-##Si el usuario carga sus cartas aparecera un nuevo menu que a parte de las opciones anteriores nos da la posibilidad,
-##de crear un mazo.
+    # Si el usuario carga sus cartas aparecera un nuevo menu que a parte de las opciones anteriores nos da la
+    # posibilidad, de crear un mazo.
     elif userinfo.user_deck_charged is False and userinfo.enemy_deck_charged is True:
         menus.enemy_deck_charged()
-##Por el contario si el usuario a decidido cargar primero las cartas del enemigo, nos apareceran las opciones de crear mazos para el enemigo,
+    # Por el contario si el usuario a decidido cargar primero las cartas del enemigo, nos apareceran las opciones de
+    # crear mazos para el enemigo,
     elif userinfo.user_deck_charged is True and userinfo.enemy_deck_charged is True:
         menus.all_decks_charged()
-##I finalmente si todas las cartas estan cargadas y esxisten los dos mazos, usuario y enemigo, podemos acceder a todas las opciones del menu,
-##incluidas las de lucha.
+# I finalmente si todas las cartas estan cargadas y esxisten los dos mazos, usuario y enemigo, podemos acceder a
+# todas las opciones del menu, incluidas las de lucha.
