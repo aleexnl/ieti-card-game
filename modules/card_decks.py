6d0d6b359254e0baa91481cf2b4da50d35aaf9dc
ikerfu-7 <ikerneira@gmail.com>
2020-03-02 11:58:58 +0100

    Eliminar funcion de validar cartas

--- modules/card_decks.py
+++ modules/card_decks.py
@@ -1,15 +1,7 @@
-def is_deck_valid(deck):  # Check if the deck has a minimum of 20 cards.
-    if len(deck) < 20:
-        return False
-    else:
-        return True
-
-
 def balanced_deck(user_deck):
     cards = user_deck.xpath('//name')
-    valid_deck = is_deck_valid(cards)
-    if valid_deck is not True:
-        return 'ERROR: No se han encontrado 20 cartas en el mazo especificado.'
     for card in range(len(cards)):
         print('carta nº: ', card + 1)
         print(cards[card].text)
+
+
