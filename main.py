febcf0fb39268bcaf2addc13ec02ea1b058e061f
Alejandro Nieto Luque <aleexnl@MacBook-Pro.local>
2020-06-11 14:51:18 +0200

    updated main

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-import sqlite3
-from os import path
-from platform import system
-from colorama import Fore, Style, init, deinit
-
-sysMsg = Fore.BLUE + '[SYSTEM] ' + Style.RESET_ALL
-infMsg = Fore.GREEN + '[INFO] ' + Style.RESET_ALL
-errMsg = Fore.RED + '[ERROR] ' + Style.RESET_ALL
-
-
-def checkSystem():
-    if system() == 'Windows':
-        init()
-
-
-def checkDatabase():
-    if path.exists('config/database.db'):
-        print(infMsg + "Found database, connecting...")
-        return databaseConnect()
-    else:
-        print(errMsg + "Database not found. Exiting")
-        if system() == 'Windows':
-            deinit()
-        exit()
-
-
-def databaseConnect():
-    db = sqlite3.connect('config/database.db')
-    print(sysMsg + "Successfully Connected to database")
-    return db
+from config import system
 
 
 def main():
-    checkSystem()  # Needed to initiate colorama in case we are using Windows
-    print(sysMsg + 'Seaching database...')
-    databaseConnection = checkDatabase
+    system.checkSystem()  # Needed to initiate colorama in case we are using Windows
+    print(system.sysMsg + 'Seaching database...')
+    databaseConnection = system.checkDatabase()
 
 
 main()
