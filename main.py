9d5a5ea0acf5f665a866134f5cd3274fd415ecc7
Alejandro Nieto Luque <aleexnl@MacBook-Pro.local>
2020-06-11 14:33:09 +0200

    Formated database connection

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import sqlite3
-import os.path
+from os import path
+from platform import system
+from colorama import Fore, Style, init, deinit
+
+sysMsg = Fore.BLUE + '[SYSTEM] ' + Style.RESET_ALL
+infMsg = Fore.GREEN + '[INFO] ' + Style.RESET_ALL
+errMsg = Fore.RED + '[ERROR] ' + Style.RESET_ALL
+
+
+def checkSystem():
+    if system() == 'Windows':
+        init()
 
 
 def dbConnect():
     db = sqlite3.connect('config/database.db')
-    print("Successfully Connected to database")
+    print(sysMsg + "Successfully Connected to database")
     return db
 
 
-if os.path.exists('config/database.db'):
-    print("Found database, connecting...")
-    dbConnection = dbConnect()
-    # print(dbConnection)
-else:
-    print("Database not found. Exiting")
-    exit()
+def main():
+    checkSystem()  # Needed to initiate colorama in case we are using Windows
+    print(sysMsg + 'Seaching database...')
+    if path.exists('/config/database.db'):
+        print(infMsg + "Found database, connecting...")
+        dbConnection = dbConnect()
+    else:
+        print(errMsg + "Database not found. Exiting")
+        if system() == 'Windows':
+            deinit()
+        exit()
 
 
-"""
-[DEV INFO] QUERY EXAMPLE:
-
-sqliteSelectQuery = "select sqlite_version();"
-cursor.execute(sqliteSelectQuery)
-record = cursor.fetchall()
-print("SQLite Database Version is: ", record)
-cursor.close()
-"""
+main()
