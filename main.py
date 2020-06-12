de05119f37a432bbde44fdfd5ca00a97450147be
Alejandro Nieto Luque <aleexnl@MacBook-Pro.local>
2020-06-12 12:13:45 +0200

    Added call to main menu

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from config import system
+from functions import system, menu
+from os import system as osSystem, name as osName
 
 
 def main():
     system.checkSystem()  # Needed to initiate colorama in case we are using Windows
+    osSystem('cls' if osName == 'nt' else 'clear')
     print(system.sysMsg + 'Seaching database...')
-    databaseConnection = system.checkDatabase()
+    databaseConnection = system.checkDatabase()  # Call database connection
+    menu.mainMenu()
 
 
 main()
