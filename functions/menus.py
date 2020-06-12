e1e7d0147628721330671b75d28639e39b95f9ba
Alejandro Nieto Luque <aleexnl@MacBook-Pro.local>
2020-06-12 14:14:21 +0200

    Changed to menus.py

--- functions/menus.py
+++ functions/menus.py
@@ -1,8 +1,10 @@
-from . import system
+from . import system, menu_options
 from colorama import Style
 
 
 def userMenu():
+    menuFunctions = {"1": "", "2": menu_options.createUser,
+                     "3": "Delete User", "4": "Show Users"}
     while True:
         print(system.infMsg + Style.BRIGHT +
               "SELECT AN OPTION:" + Style.RESET_ALL)
@@ -11,5 +13,6 @@ def userMenu():
         print("[3]. Delete an user")
         print("[4]. Show all users")
         print("[Q]. Quit")
-        option = str(input(system.usrMsg))
-        system.checkOption(option)
+        option = str(input(system.usrMsg))  # Input user option of the menu
+        # Check option and menu options
+        system.checkOption(option, menuFunctions)
