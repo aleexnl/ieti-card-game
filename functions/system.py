500974428d98ba4bc62d5c2333499c20236da32e
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-06-13 14:43:10 +0200

    Added function to check inputs

--- functions/system.py
+++ functions/system.py
@@ -59,6 +59,13 @@ def check_option(option, menu):
         print(warMsg + "Option not found, please input a valid option.")
 
 
+def check_str_input(string):
+    if len(string) <= 0:
+        print(errMsg + "No text provided.")
+    else:
+        return True
+
+
 def exec_query(db, query):
     """
     Function to execute query.
@@ -67,3 +74,17 @@ def exec_query(db, query):
     cursor = db.cursor()
     cursor.execute(query)
     return cursor.fetchall()
+
+
+def insert_new_user(db, values):
+    """
+    Function to execute query.
+    It may disappear depending on project's evolution.
+    """
+    sql = "INSERT INTO users(username, points) VALUES(?,?);"
+    cursor = db.cursor()
+    print(sysMsg + "Adding, user to database...")
+    cursor.execute(sql, values)
+    db.commit()
+    print(infMsg + "User created correctly!")
+    cursor.close()
